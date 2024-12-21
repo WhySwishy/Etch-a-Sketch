1821f64f24d0367ff93d22928e457518fe61e0fb
Swishy <swishyeth@gmail.com>
2024-12-21 13:45:47 +0530

    grid now changes and add divs using slider

--- sketch.js
+++ sketch.js
@@ -8,4 +8,27 @@ slider.addEventListener('input', () => {
   sliderValues.forEach((element) => {
     element.textContent = slider.value;
   });
+  creategriditems(slider.value);
 });
+
+// Changing the grid values using slider.value
+
+const sketcharea = document.querySelector('.sketch-area');
+let size = slider.value;
+
+function creategriditems(size){
+  const totalitems = size * size;
+
+  sketcharea.innerHTML = ' '
+
+  for (let i = 1; i <= totalitems; i++){
+    const newitem = document.createElement('div');
+    newitem.className = 'grid-item';
+    sketcharea.appendChild(newitem);
+  }
+
+  sketcharea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  sketcharea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+}
+
+creategriditems(slider.value); 
